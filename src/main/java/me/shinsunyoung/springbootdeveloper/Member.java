9b451bb8fdb5bb1a2d404f18e54f512b71a60c4e
kikicom <kihwan0624@hotmail.com>
2023-07-24 18:02:53 +0900

    project init

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/Member.java
@@ -0,0 +1,20 @@
+package me.shinsunyoung.springbootdeveloper;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;            //DB테이블의 'ID' 컬럼과 매칭
+    @Column(name = "name", nullable = false)
+    private String name;        //DB 테이블의 'name' 컬럼과 매칭
+}
